c276bbc2667cb8a464c582ee58bb60ddee96e13b
natsumi <aka.723n@gmail.com>
2022-04-08 10:12:49 +0900

    Header,Content,Footer追加

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Header} from "./Content/Header";
+import {Content} from "./Content/Content";
+import {Footer} from "./Content/Footer";
+
+
+export const App = () => {
+  return(
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+};
