28af6c3705c1b0e37474e80a9d90e4047d26543a
natsumi <aka.723n@gmail.com>
2022-04-08 12:00:52 +0900

    add GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import React from "react";
 import {Header} from "./Content/Header";
 import {Content} from "./Content/Content";
 import {Footer} from "./Content/Footer";
+import {createGlobalStyle} from "styled-components";
 
 
 export const App = () => {
   return(
     <>
+      <GlobalStyle />
       <Header />
       <Content />
       <Footer />
     </>
   );
 };
+
+const GlobalStyle = createGlobalStyle`
+  #root {
+    font-family: 'Noto Sans JP', sans-serif;
+    
+  }
+  #root li{
+    list-style: none;
+  }
+
+  a{
+    text-decoration: none;
+    color: rgb(51,51,51);
+  }
+  img{
+    max-width: 100%;
+  }
+
+`;
