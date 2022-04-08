944422e706f120f08b89afdef0b20f38f319383c
natsumi <aka.723n@gmail.com>
2022-04-08 11:23:24 +0900

    HeaderTop追加

--- src/Content/Header.jsx
+++ src/Content/Header.jsx
@@ -1,9 +1,22 @@
-
+import styled from "styled-components";
+import logoImg from "../assets/images/logo.png"
 
 export const Header = () =>{
   return(
-    <div>
-      Header
-    </div>
+    <HeaderTop>
+      <div className="header">
+        <img src={logoImg} alt=""/>
+        <ul>
+          <li>totonoiについて</li>
+          <li>お問い合わせ</li>
+        </ul>
+      </div>
+    </HeaderTop>
   );
 };
+
+const HeaderTop = styled.div`
+  max-height: 100px;
+  
+  
+`;
