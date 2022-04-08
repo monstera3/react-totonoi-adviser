a2f9b3f8aa71fd83bc76262fdf45601cf88f6f37
natsumi <aka.723n@gmail.com>
2022-04-08 12:02:34 +0900

    change HeaderTop css

--- src/Content/Header.jsx
+++ src/Content/Header.jsx
@@ -17,6 +17,7 @@ export const Header = () =>{
 
 const HeaderTop = styled.div`
   max-width: 1440px;
+  margin: 0 auto;
   .header{
     display: flex;
     justify-content: space-between;
@@ -26,6 +27,7 @@ const HeaderTop = styled.div`
     }
     ul{
       display: flex;
+      justify-content: space-between;
     }
   }
   
