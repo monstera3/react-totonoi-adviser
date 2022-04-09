17675958a6cf08bf20e9ca2218f008b925b33d9a
natsumi <aka.723n@gmail.com>
2022-04-09 11:38:34 +0900

    add title,catch-copy

--- src/components/content/QuestionStart.jsx
+++ src/components/content/QuestionStart.jsx
@@ -4,12 +4,21 @@ export const QuestionStart = (props) =>{
 
   return(
     <QuestionStartStyle>
+      <p className="title">totonoi</p>
+      <p className="catch-copy">自律神経を整える</p>
       <button onClick={props.onClick}>診断 START</button>
     </QuestionStartStyle>
   );
 };
 
 const QuestionStartStyle = styled.div`
+  text-align: center;
+  .title{
+    color: white;
+  }
+  .catch-copy{
+    font-size: 2rem;
+  }
   button{
     padding:1% 3%;
     color: #666;
