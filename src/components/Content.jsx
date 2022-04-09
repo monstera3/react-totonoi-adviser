cd3796d51c644f1acd449f529aa6ee394fc2c8bf
natsumi <aka.723n@gmail.com>
2022-04-09 10:55:41 +0900

    ContentImg定義

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import hereImg from "../assets/images/hero.png"
+import question1Img from "../assets/images/question1.png"
 import {useState} from "react";
 import {QuestionNo1} from "./content/QuestionNo1";
 import {QuestionStart} from "./content/QuestionStart";
@@ -14,7 +15,7 @@ export const Content = () =>{
 
   const ContentTopAndButton = () => {
     return (
-      <ContentStyle>
+      <ContentStyle img={ContentImg(questionFlag)}>
         {questionFlag
           ? <QuestionStart onClick={onClickSwitchQuestionFlag} />
           : <QuestionNo1 />
@@ -31,8 +32,19 @@ export const Content = () =>{
   );
 };
 
-const ContentStyle = styled.div`
-  background: url(${hereImg}) no-repeat center /cover;
+const ContentImg = (questionFlag) => {
+  switch (questionFlag) {
+    case true:
+      return hereImg;
+    case false:
+      return question1Img;
+    default:
+      return null;
+  }
+}
+
+const ContentStyle =  styled.div`
+  background: url(${props => props.img}) no-repeat center /cover;
   min-height: 700px;
   display: flex;
   justify-content: center;
