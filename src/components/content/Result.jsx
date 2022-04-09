ceb20373d949439592d39529de8bc9e4829b1d67
natsumi <aka.723n@gmail.com>
2022-04-09 18:30:29 +0900

    Result.jsx css編集

--- src/components/content/Result.jsx
+++ src/components/content/Result.jsx
@@ -8,11 +8,12 @@ export const Result = () =>{
       <div className="result-content">
         <h2>today’s   totonoi   point</h2>
         <div className="text-content">
-          <h3>「座っている時間」を <br/>
+          <h3>「座っている時間」を<br/>
             できるだけ短くする</h3>
           <p>説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト</p>
+
         </div>
-        <img src={resultImg} alt=""/>
+        {/*<img src={resultImg} alt=""/>*/}
       </div>
 
     </ResultStyle>
@@ -20,12 +21,26 @@ export const Result = () =>{
 };
 
 const ResultStyle = styled.div`
+  background: #f8f8f8;
+  height: 700px;
   .result-content{
-    
-    
-    .text-content{
-      
+    display: grid;
+    grid-template-columns: 400px 500px;
+    justify-content: center;
+    padding: 8% 3%;
+    h2{
+      font-size: 13px ;
     }
+    .text-content{
+      background: white;
+      margin-top: 100px;
+      padding: 0 100px 135px;
+      h3{
+        font-size: 24px;
+      }
+    }
+    //img{
+    //  width: 500px;
+    //}
   }
-  
 `;
