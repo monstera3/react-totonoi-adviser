957506d3ef1312107362a637d917fdb6ca1b8f04
natsumi <aka.723n@gmail.com>
2022-04-12 21:06:32 +0900

    chore: 質問3の回答を追加

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -95,6 +95,7 @@ export const Content = () =>{
         const question = QUESTION_LIST[questionIndex];
         return (<Question answerQuestion={answerQuestion} question={question} />)
       //質問３.currentQuestionNumberはどこを指している？
+      //  const [currentQuestionNumber,setCurrentQuestionNumber] で定義したもの
       case "finished":
         return (<Result text={resultText()}/>)
       default:
