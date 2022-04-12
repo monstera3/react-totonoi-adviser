cdc9e6f1a33eea3df8d36e2034c69e6df0b00d40
natsumi <aka.723n@gmail.com>
2022-04-12 21:05:45 +0900

    chore: 質問2の回答を追加

--- src/components/content/Question.jsx
+++ src/components/content/Question.jsx
@@ -13,6 +13,7 @@ export const Question = (props) =>{
         {/*質問１.{props.question.title}はcontentの91行目にとぶ？*/}
         {/* <Question hoge={aaaa}> のように定義するとQuestionコンポーネントの内部で {props.hoge} として取得できる */}
         {/*質問２.↓の['A']はどういう意味？*/}
+        {/* QUESTION_LIST の構造をシンプルに変えて不要にしました */}
         <button onClick={() => props.answerQuestion('A')} className="a-answer">A.  {props.question.options[0]}</button>
         <button onClick={() => props.answerQuestion('B')} className="b-answer">B.  {props.question.options[1]}</button>
       </div>
